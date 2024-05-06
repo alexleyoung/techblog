ce5ec071c9ff9a15cfd9be30ca0e32fabdbe4ad6
alx-yng <alexyoung0119@gmail.com>
2024-05-06 16:50:22 -0500

    start articles page

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
+
 const Articles = () => {
   // data = getData
   // data.map((post)=>{
   //   return <Article post={post} />
   // })
 
-  return <div>Articles</div>;
+  return (
+    <div>
+      {/* {data.map((post) => {
+        return <Article post={post} />;
+      })} */}
+    </div>
+  );
 };
+
+// function article({ post }) {
+//   return <Link href={`/articles/${post.title_slug}`}>{post.title}</Link>;
+// }
 
 export default Articles;
