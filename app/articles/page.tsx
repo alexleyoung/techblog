784243ed37468c58f6e0900662b885145543de6c
kaylee535 <kaymaczek@gmail.com>
2024-05-06 21:09:51 -0500

    articles page!!!

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,22 +1,54 @@
 import Link from "next/link";
+import * as React from "react";
 
-const Articles = () => {
-  // data = getData
-  // data.map((post)=>{
-  //   return <Article post={post} />
-  // })
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getPosts } from "@/lib/api/posts";
+
+const Articles = async () => {
+  const data = await getPosts();
+
+  function Article({ post }: { post: any }) {
+    return (
+      <Link href={`/articles/${post.title_slug}`}>
+        <Card className="w-[700px] m-3  ">
+          <CardHeader>
+            <CardTitle>{post.title}</CardTitle>
+            <CardDescription>{post.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>{post.content}</p>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <p>{post.timestamp}</p>
+            <p>{post.author}</p>
+          </CardFooter>
+        </Card>
+      </Link>
+    );
+  }
 
   return (
     <div>
-      {/* {data.map((post) => {
-        return <Article post={post} />;
-      })} */}
+      <h1 className="text-4xl text-center m-9">Articles</h1>
+      <div className="flex flex-col items-center">
+        {data.map((post) => {
+          return <Article post={post} />;
+        })}
+      </div>
+      <Link href="/">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3">
+          Back to home
+        </button>
+      </Link>
     </div>
   );
 };
-
-// function article({ post }) {
-//   return <Link href={`/articles/${post.title_slug}`}>{post.title}</Link>;
-// }
 
 export default Articles;
